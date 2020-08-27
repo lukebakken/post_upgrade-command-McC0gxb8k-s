bdfb28f3d3605f697f29d26ba597d46d0bb52ad2
Luke Bakken <luke@bakken.io>
2020-08-26 18:26:46 -0700

    add msg count

--- run-perf-test.sh
+++ run-perf-test.sh
@@ -4,6 +4,7 @@ set -o errexit
 set -o nounset
 
 declare -r host=localhost
+declare -ri msg_count=1024
 declare -i port=5672
 
 for ((i = 0; i < 11; i++))
@@ -11,7 +12,7 @@ do
     port="$((5672 + (i % 3)))"
     queue="test_$i"
     uri="--uri amqp://$host:$port"
-    (cd $HOME/development/rabbitmq/rabbitmq-perf-test && mvn exec:java -Dexec.mainClass='com.rabbitmq.perf.PerfTest' -Dexec.args="$uri --predeclared --queue test_$i --flag persistent --producers 1 --consumers 0 --size 1024 --pmessages 333333") &
+    (cd $HOME/development/rabbitmq/rabbitmq-perf-test && mvn exec:java -Dexec.mainClass='com.rabbitmq.perf.PerfTest' -Dexec.args="$uri --predeclared --queue test_$i --flag persistent --producers 1 --consumers 0 --size 1024 --pmessages $msg_count") &
 done
 wait
 echo Done.
