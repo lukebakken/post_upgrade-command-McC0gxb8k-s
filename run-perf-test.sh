9ad78d0aea28d7f6aa17f4fc2d4e8ef157353f0e
Luke Bakken <luke@bakken.io>
2020-08-27 13:15:08 -0700

    simplify

--- run-perf-test.sh
+++ run-perf-test.sh
@@ -3,9 +3,10 @@
 set -o errexit
 set -o nounset
 
-declare -ri msg_count=333333
+declare -ri msg_count="${1:-333333}"
+echo "[INFO] msg_count $msg_count"
 
 cd "$HOME/development/rabbitmq/rabbitmq-perf-test"
-mvn exec:java -Dexec.mainClass='com.rabbitmq.perf.PerfTest' -Dexec.args="--uris amqp://localhost:5672,amqp://localhost:5673,amqp://localhost:5674  --predeclared --queue-pattern test_%d --queue-pattern-from 0 --queue-pattern-to 11 --flag persistent --producers 12 --consumers 0 --size 1024 --pmessages $msg_count"
+mvn exec:java -Dexec.mainClass='com.rabbitmq.perf.PerfTest' -Dexec.args="--uris amqp://localhost:5672,amqp://localhost:5673,amqp://localhost:5674 --auto-delete false --predeclared --queue-pattern test_%d --queue-pattern-from 0 --queue-pattern-to 11 --flag persistent --producers 12 --consumers 0 --size 1024 --pmessages $msg_count"
 
 echo Done.
--- setup-env.sh
+++ setup-env.sh
@@ -6,16 +6,14 @@ set -o nounset
 declare -r host=localhost
 declare -i port=15672
 
+echo -n Declaring ha-three policy...
+rabbitmqadmin -H $host -P $port declare policy name=ha-three pattern='^test_\d+$' definition='{"queue-mode":"lazy","ha-mode":"all","ha-sync-mode":"automatic"}' priority=0 apply-to=queues
+
 for ((i = 0; i < 11; i++))
 do
     port="$((15672 + (i % 3)))"
     queue="test_$i"
 
     echo -n "Declaring queue $queue..."
-    rabbitmqadmin -H $host -P $port declare queue name="$queue" durable=true
-    echo Done.
+    rabbitmqadmin -H $host -P $port declare queue name="$queue" durable=true auto_delete=false
 done
-
-echo -n Declaring ha-three policy...
-rabbitmqadmin -H $host -P $port declare policy name=ha-three pattern='^test_\d+$' definition='{"queue-mode":"lazy","ha-mode":"all","ha-sync-mode":"automatic"}' priority=0 apply-to=queues
-echo Done.
